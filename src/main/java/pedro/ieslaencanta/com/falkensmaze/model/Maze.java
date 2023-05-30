cdf7ac2543c4cc37f7b1b3b0f05f0a5d8842e361
Jose Navarro <chemanavrod@gmail.com>
2023-05-30 14:51:46 +0200

    caracola

---
+++ src/main/java/pedro/ieslaencanta/com/falkensmaze/model/Maze.java
@@ -0,0 +1,170 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pedro.ieslaencanta.com.falkensmaze.model;
+
+import com.google.gson.Gson;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.Unmarshaller;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Serializable;
+
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+import pedro.ieslaencanta.com.falkensmaze.Size;
+
+/**
+ *
+ * @author Pedro
+ */
+@XmlRootElement
+public class Maze {
+
+    private Size size;
+    private Block[][] blocks;
+    private double time;
+    private String sound;
+
+    public Maze() {
+    }
+
+    public void init() {
+        this.setBlocks(new Block[this.getSize().getHeight()][this.getSize().getWidth()]);
+        for (int i = 0; i < this.getBlocks().length; i++) {
+            for (int j = 0; j < this.getBlocks()[i].length; j++) {
+                this.getBlocks()[i][j] = new Block();
+
+            }
+        }
+    }
+
+    public void reset() {
+        for (int i = 0; i < this.getBlocks().length; i++) {
+            for (int j = 0; j < this.getBlocks()[i].length; j++) {
+                this.getBlocks()[i][j] = null;
+
+            }
+        }
+        this.setBlocks(null);
+    }
+
+    public void reset(Size newsize) {
+        this.reset();;
+        this.setSize(newsize);
+        this.init();
+    }
+
+    public void setBlockValue(String value, int row, int col) {
+        this.getBlocks()[col][row].setValue(value);
+    }
+
+    public String getBlockValue(int row, int col) {
+        return this.getBlocks()[row][col].getValue();
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+
+    public Block[][] getBlocks() {
+        return blocks;
+    }
+
+    public void setBlocks(Block[][] blocks) {
+        this.blocks = blocks;
+    }
+
+    public static Maze load(File file)  {
+        String extension = file.getName().substring(file.getName().lastIndexOf(".") + 1);
+      
+        return null;
+
+    }
+
+    public static void save(Maze maze, File file) {
+        if (maze.sound == null || maze.sound.equals("")) {
+            throw new Exception("Es necesario indicar el sonido del laberinto");
+        }
+      
+    }
+
+    private static Maze loadJSON(File file)  {
+        return null;
+    }
+
+    private static Maze loadXML(File file)  {
+
+        return null;
+          
+    }
+
+    public static Maze loadBin(File file)  {
+      FileInputStream os = new FileInputStream(file);
+      
+      ObjectInputStream oos = new ObjectInputStream(os);
+      Maze m = (Maze) oos.readObject();
+      oos.close();
+      os.close();
+      
+      
+        return m;
+    }
+
+    private static void saveJSON(Maze maze, File file)  {
+      
+    }
+
+    private static void saveXML(Maze maze, File file)  {
+      
+
+    }
+
+    public static void saveBin(Maze maze, File file)  {
+      FileOutputStream os = new FileOutputStream(file);
+      
+      ObjectOutputStream oos = new ObjectOutputStream(os);
+      Maze m = (Maze) oos.writeObject(oos);
+      oos.close();
+      os.close();
+    }
+
+}
